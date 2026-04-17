7031d8127baa0f5fb3653bd870ac3800ea31d0c2
Ankit Mudi <145973542+Ankit-mudi@users.noreply.github.com>
2026-04-17 18:07:45 +0530

    Update 55. Jump Game.java

--- Arrays/55. Jump Game.java
+++ Arrays/55. Jump Game.java
@@ -1,1 +1,15 @@
 
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int finalPos = nums.length-1;
+        for(int i=nums.length-2; i>=0; i--)
+        {
+            if(i+nums[i] >= finalPos){
+                finalPos = i;
+            }
+        }
+        //If we reach the final index, then we can make the jump possible
+        return finalPos==0;
+    }
+}
