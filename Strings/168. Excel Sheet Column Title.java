cee639ea0ec439819b87898609de858a787250b8
Ankit Mudi <145973542+Ankit-mudi@users.noreply.github.com>
2026-04-18 20:57:02 +0530

    Update 168. Excel Sheet Column Title.java

--- Strings/168. Excel Sheet Column Title.java
+++ Strings/168. Excel Sheet Column Title.java
@@ -1,1 +1,14 @@
+class Solution{
+  Public String convertToTile(int colNo)
+  {
+    StringBuilder sb = new StringBuilder();
 
+    while(colNo > 0)
+      {
+          int rem = (colNo-1) % 26;
+          sb.append((char)(rem+'A'));
+          colNo = (colNo-1)/26;
+      }
+      return sb.reverse().toString();
+  }
+}
