ad7abca3007d07b48cb3038b5c83c4b7b1914bd8
Ankit Mudi <145973542+Ankit-mudi@users.noreply.github.com>
2026-04-18 21:35:17 +0530

    Update 171. Excel Sheet Column Number.java

--- Strings/171. Excel Sheet Column Number.java
+++ Strings/171. Excel Sheet Column Number.java
@@ -1,1 +1,15 @@
 
+public class Solution()
+{
+    public int TitleToNumber(String col)
+      {
+          int sum = 0, p = 0;
+
+          for(int i=col.length()-1; i>=0; i--)
+            {
+                int tmp = col.charAt(i) - 64;
+                sum = sum + (int)Math.pow(26, p++) * tmp;
+            }
+            return sum;
+      }
+}
